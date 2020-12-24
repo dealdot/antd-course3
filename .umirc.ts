03c92db9d23192756b692263a5235cccba87a123
dealdot <dealdot@163.com>
2020-12-24 13:50:50 +0800

    umirc.ts add from main branch

--- .umirc.ts
+++ .umirc.ts
@@ -4,7 +4,8 @@ export default defineConfig({
   nodeModulesTransform: {
     type: 'none',
     age:11,
-    year: 2020
+    year: 2020,
+    month: 3
   },
   routes: [
     { path: '/', component: '@/pages/index' },
