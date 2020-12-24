f38ff2d4e28aaf738de66b6b6a544a0998bb7aa7
dealdot <zonglw@deepblueai.com>
2020-12-24 10:10:54 +0800

    modified umirc.ts

--- .umirc.ts
+++ .umirc.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'umi';
 export default defineConfig({
   nodeModulesTransform: {
     type: 'none',
+    age:11
   },
   routes: [
     { path: '/', component: '@/pages/index' },
