eb3cc3dae3d5f15cc7742f7bc1fa1830e7029312
dealdot <dealdot@163.com>
2020-12-24 15:11:39 +0800

    add .umirc.ts many times

--- .umirc.ts
+++ .umirc.ts
@@ -6,7 +6,8 @@ export default defineConfig({
     age:11,
     year: 2020,
     month: 3,
-    day: 333
+    day: 333,
+    hour: 11
   },
   routes: [
     { path: '/', component: '@/pages/index' },
