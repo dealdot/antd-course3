42b832463b72316b9ce64d60fdce78a0dfcfb8a2
dealdot <dealdot@163.com>
2020-12-24 14:10:39 +0800

    .umirc.ts updated from master again

--- .umirc.ts
+++ .umirc.ts
@@ -5,7 +5,8 @@ export default defineConfig({
     type: 'none',
     age:11,
     year: 2020,
-    month: 3
+    month: 3,
+    day: 333
   },
   routes: [
     { path: '/', component: '@/pages/index' },
