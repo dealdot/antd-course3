5e629137189f5cef14b37257e6e32d28bd4d8be3
dealdot <dealdot@163.com>
2020-12-28 10:14:46 +0800

    test again

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ import styles from './index.less';
 export default () => {
   return (
     <div>
-      <h1 className={styles.title}>Page index h1 abc efg 111</h1>
+      <h1 className={styles.title}>Page index h1 abc efg 666</h1>
     </div>
   );
 }
